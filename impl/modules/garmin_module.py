0f46f0cc525492def46b988a97950e884c400c54
Joffroi Holcombe <joffroi.holcombe@gmail.com>
2022-06-12 19:26:31 -0500

    adding a better comment.

--- impl/modules/garmin_module.py
+++ impl/modules/garmin_module.py
@@ -4,4 +4,4 @@ import time
 class GarminModule: 
     def __init__(self, config):
         self.invalid = False
-        print("Garmin Modeul Test")
+        print("Garmin Module has been initiated")
