7325406e9a58db4c34f0d06133f837bccfaf172b
Joffroi Holcombe <joffroi.holcombe@gmail.com>
2022-06-13 19:16:30 -0500

    Testing logging into account

--- impl/modules/garmin_module.py
+++ impl/modules/garmin_module.py
@@ -1,7 +1,24 @@
 
 import time
+import datetime
+
+from garminconnect import (
+    Garmin,
+    GarminConnectConnectionError,
+    GarminConnectTooManyRequestsError,
+    GarminConnectAuthenticationError,
+)
 
 class GarminModule: 
     def __init__(self, config):
         self.invalid = False
         print("Garmin Module has been initiated")
+
+        ## Initialize Garmin api with your credentials
+        
+        client_id = config['Garmin']['email']
+        client_password = config['Garmin']['password']
+        api = Garmin(client_id, client_password)
+        api.login()
+
+        print(api.get_full_name())
