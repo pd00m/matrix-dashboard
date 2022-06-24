419cecc1f7513f768079fe324563476582f0697c
Joffroi Holcombe <joffroi.holcombe@gmail.com>
2022-06-23 19:53:36 -0500

    adding logs because math didn't work

--- impl/apps_v2/garmin_screen.py
+++ impl/apps_v2/garmin_screen.py
@@ -163,9 +163,11 @@ def displaySleepRetangles(sleep, total_sleep, draw, self):
             datetime.strptime(level["endGMT"], "%Y-%m-%dT%H:%M:%S.%f")
             - datetime.strptime(level["startGMT"], "%Y-%m-%dT%H:%M:%S.%f")
         ).total_seconds()
-        # print(str(length))
+        print("Length: " + str(length))
+        print("Total: " + str(total_sleep))
         fillRatio = math.floor((total_sleep - length) / self.canvas_width)
-        print(str(fillRatio))
+        print("Ratio: " + str(fillRatio))
+        time.sleep(5)
         if fillRatio >= 1:
             xmax = xmin + fillRatio
             draw.rectangle(
