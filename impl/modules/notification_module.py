44c6df0f1cc2e6916254a29a21c3b03327f0cd4f
Joffroi Holcombe <joffroi.holcombe@gmail.com>
2022-06-28 06:51:30 -0500

    Actually, error might have been the list reference

--- impl/modules/notification_module.py
+++ impl/modules/notification_module.py
@@ -95,9 +95,9 @@ def on_message(_, message, noti_queue, app_white_list):
             )
         elif contents["type"] == "sms_changed" and len(contents["notifications"]) > 0:
             print("SMS received!")
-            print(contents["notifications"]["thread_id"])
-            print(contents["notifications"]["title"])
-            print(contents["notifications"]["body"])
+            print(contents[0]["notifications"]["title"])
+            print(int(contents[0]["notifications"]["thread_id"]))
+            print(contents[0]["notifications"]["body"])
             noti_queue.put(
                 Notification(
                     "SMS",
