1eff4d6b5de2d4344e310cd825fe9a553ab857bb
Joffroi Holcombe <joffroi.holcombe@gmail.com>
2022-08-27 14:09:37 -0500

    not getting permissions on scopes
    swapping to spaces

--- impl/modules/spotify_module.py
+++ impl/modules/spotify_module.py
@@ -27,7 +27,7 @@ class SpotifyModule:
                     os.environ["SPOTIPY_CLIENT_SECRET"] = client_secret
                     os.environ["SPOTIPY_REDIRECT_URI"] = redirect_uri
 
-                    scope = "user-read-currently-playing,user-read-playback-state,user-modify-playback-state"
+                    scope = "user-read-currently-playing user-read-playback-state user-modify-playback-state"
                     self.auth_manager = spotipy.SpotifyOAuth(
                         client_id,
                         client_secret,
